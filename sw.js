351323a2ed493493157e1632c911f2f8e8764d1c
tória ☆彡 <vitoriaoliveiramedeiros2006@gmail.com>
2023-09-02 17:56:34 -0400

    commit inicial

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = "jogodavelha";
+const filesToCache = ["/", "/index.html", "/css/style.css"];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(filesToCache)
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
